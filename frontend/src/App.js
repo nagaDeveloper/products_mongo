8b05f2202582bbd6dfdf04b2cd73931abdd2e6d7
nagaDeveloper <naga.developer@gmail.com>
2024-04-28 00:09:03 +0530

    add frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+function App() {
+  return (
+    <React.Fragment>
+      <div className="App">
+        <h1>hi naaga</h1>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
